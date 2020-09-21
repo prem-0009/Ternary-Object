0eb2344f5e61f989f90ff722c2b8dd97d7ca8636
prem <prem.sherpa@codeimmersives.com>
2020-09-20 21:16:13 -0400

    tenary

--- main.js
+++ main.js
@@ -11,41 +11,43 @@ const oneGreeting = function (arr) {
     return item.name ? `Hello ${item.name}` : `Hello Stranger`;
   });
 };
-console.log("one:\n", oneGreeting(person),'\n\n');
+console.log("one:\n", oneGreeting(person), "\n\n");
 
 //2.
 ///Create a function that uses ternary to decide whether the person can drive
 // If they are below 15 then log 'cannot drive', if they are 15 then log 'can drive with a parent', if they are 16 or above then 'yes they can drive'
-const twoCanDrive=(age)=>{
-  return age < 15 ? 'cannot drive' 
-     : age === 15 ? 'can drive with parent'
-     : 'yes they can drive'
-}
+const twoCanDrive = (age) => {
+  return age < 15
+    ? "cannot drive"
+    : age === 15
+    ? "can drive with parent"
+    : "yes they can drive";
+};
 
-console.log('two: \n',twoCanDrive(16), '\n\n')
-
+console.log("two: \n", twoCanDrive(16), "\n\n");
 
 //3.
 // create a variable speedMessage that has a value resulting from a ternary
-let speedMessage = (speed)=>{
-// if the speed equals 70 it should say '70 is the perfect speed'
-let allowableSpeed = [71,72,73,74,78,79];
-return speed === 70 ? `${speed} is the perfect speed`
-: speed >= 90 ? `Are you trying to get us killed`
-: speed <  50 ? `Faster please`
-: speed < 10   ? 'You should not drive'
-: speed >= allowableSpeed[0] && speed <= allowableSpeed[allowableSpeed.length-1] ? `${speed} is decent speed`
-: 'hi'
-// if the speed is greater than or equal to 90 it should say 'Are you trying to get us killed'
-// if the speed is less than 50 it should have the value 'Faster please'
-// if speed is less than 10 it should say you should not drive
-// create a variable allowableSpeed that sets a speed based on the range of numbers that are left.
-//so...if allowableSpeed is set to a number in that range your default output should be `<speed> is a decent speed`
-// ---
-}
+let speedMessage = (speed) => {
+  // if the speed equals 70 it should say '70 is the perfect speed'
+  let allowableSpeed = "a decent speed";
+  return speed >= 90
+    ? `Are you trying to get us killed`
+    : speed < 50
+    ? `Faster please`
+    : speed < 10
+    ? "You should not drive"
+    : // : speed >= allowableSpeed[0] && speed <= allowableSpeed[allowableSpeed.length - 1]  ? `${speed} is decent speed`
+      `${speed} is ${allowableSpeed}`;
+  // if the speed is greater than or equal to 90 it should say 'Are you trying to get us killed'
+  // if the speed is less than 50 it should have the value 'Faster please'
+  // if speed is less than 10 it should say you should not drive
+  // create a variable allowableSpeed that sets a speed based on the range of numbers that are left.
+  //so...if allowableSpeed is set to a number in that range your default output should be `<speed> is a decent speed`
+  // ---
+};
 
-
-console.log('three\n', speedMessage(77), '\n\n')
+console.log("three\n", speedMessage(77), "\n\n");
 //4.
 // map through an array and if the value is greater than 50 it goes in the arr1
 //if the array is less than 50 it goes in arr2
@@ -69,17 +71,21 @@ let arr = [
   NaN,
   44.98999,
 ];
+
 let arr1 = [];
 let arr2 = [];
 let arr3 = [];
-const four = arr.map((item)=>
-  item > 50    ? arr1.push(Math.round(item))
-  : item < 50  && typeof item === 'number'  ? arr2.push(Math.round(item))//????????????????? need to put 2 digit after decimal
-  :  typeof item=== 'string' ? arr3.push(item)
-  : 'hello'
-)
+const four = arr.map((item) =>
+  item > 50
+    ? arr1.push(Math.round(item))    : 
+    item < 50 && typeof item === "number"
+    ? arr2.push(item.toString().split('').splice(0,5).join('')) //????????????????? need to put 2 digit after decimal
+    : typeof item === "string"
+    ? arr3.push(item)
+    : "hello"
+);
 // item !== undefined && item !== null && item !== NaN
-console.log('four: \n', arr1, arr2, arr3,'\n\n')
+console.log("four: \n", arr1, arr2, arr3, "\n\n");
 
 // Object Methods:
 5; //Use object methods to solve problem
@@ -92,55 +98,35 @@ let prices = {
 
 // console.log(typeof prices)
 
-const priceEntries = Object.entries(prices)
-.map((item, i, arr)=>{
-  if (typeof item[1] === 'number'){
-    item[1] = item[1]*2;
-    return item
-  }
-  // console.log(item[0])
-  
-  if (typeof item[1] === 'object'){
-    const subObject = Object.entries(item[1])
-    // console.log(item[0])
-    .map((item1)=>{
-      // console.log(item1[1]*2)
-      item1[1] = item1[1] * 2
-      // console.log(item1)
-      return item1
-    
-    })
+const priceEntries = Object.entries(prices).map((item, i, arr) => {
+  // console.log(item)
 
-    console.log('subobject', subObject)
-    const newitem1 = Object.fromEntries(subObject)
-    console.log('newitem1', newitem1)
-    // console.log('hi',item[0])
-    // let result{} = item[0];
-
-
-
-    console.log({[item[0]]:newitem1})
-    // subObject.unshift(item[0])
-    return (
-      [item[0]]
-      // ,
-      //  newitem1
-      , subObject
-       );
-    
+  if (typeof item[1] === "number") {
+    item[1] = item[1] * 2;
+    return item;
   }
 
-  
-})
+  if (typeof item[1] === "object") {
+    let hi = {};
 
+    const newSubObj = Object.entries(item[1]).map((element, i, newArr) => {
+      element[1] *= 2;
+      hi[element[0]] = element[1];//creating an object is the goal.
+      return ;//doesn't matter the outcome
+    });
+    // console.log({[item[0]]:hi})
+    console.log([item[0], hi])
+    return [item[0], hi];//returning an array with an obj inside
+  }
+  //hard coding 
+  // if (typeof item[1] === "object") {
+  //   item[1].lg *= 2;
+  //   item[1].sm *= 2;
+  //   return item;
+  //   }
+});
+// console.log("\nasd;f\n", priceEntries);
 
-const newObj = Object.fromEntries(priceEntries)
-console.log('\nasd;f\n', priceEntries)
-console.log('\n\nfive:\n' ,newObj)
-console.log('\n\nreplace the if with tenary and \n??????????????put back in the final object')
-
-
-
-
-
+const newObj = Object.fromEntries(priceEntries);
+console.log("\n\nfive:\n", newObj);
 
